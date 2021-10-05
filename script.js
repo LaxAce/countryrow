af74cd8f86755603eb4bf8a7c22d9c690d28d14f
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2021-10-05 17:59:56 +0100

    more functionality

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 "use strict";
 
 let countries;
+const activeCountry = document.querySelector(".country-column");
+const firstPage = document.querySelector(".first-page");
+const secondPage = document.querySelector(".second-page");
+const detailed = document.querySelector(".full-details");
+const arrowBack = document.querySelector(".arrow-back");
+const countryName = document.querySelector(".input");
+
+// Fuctions
+const togglePage = () => {
+  firstPage.classList.toggle("hidden");
+  secondPage.classList.toggle("hidden");
+};
 
 fetch("https://restcountries.com/v2/all")
   .then((response) => response.json())
   .then((data) => {
-    // console.log(data);
     init(data);
   })
-  .catch((err) => {
-    console.error(err);
-  });
+  .catch((err) => console.error(err));
 
 function init(countriesData) {
   console.log(countriesData.length);
@@ -18,6 +27,7 @@ function init(countriesData) {
   countries = countriesData;
   let options = "";
   let semiDetails = "";
+  let fullDetails = "";
   let continent = [];
   let sortedContinent = [""];
 
@@ -35,36 +45,11 @@ function init(countriesData) {
 
   document.getElementById("region").innerHTML = options;
 
-  // document.getElementById("region").addEventListener("click", function () {
-  //   console.log("yess yess");
-
-  //   const byRegion = countries.filter((country) => country.region == "Asia");
-
-  //   for (let i = 0; i < byRegion.length; i++) {
-  //     semiDetails += ` <div class="country-column">
-  //     <div class="img-box"><img src="${byRegion[i].flags.svg}" alt="countryFlag" height="100px" width="200px" /></div>
-  //     <div class="country-details">
-  //       <h3>${byRegion[i].name}</h3>
-  //       <div>
-  //         <label for="population">Population:</label> <span>${byRegion[i].population}</span>
-  //       </div>
-  //       <div><label for="region">Region:</label> <span>${byRegion[i].region}</span>
-  //       </div>
-  //       <div> <label for="capital">Capital:</label>
-  //       <span>${byRegion[i].capital}</span></div>
-
-  //     </div>
-  //   </div>`;
-  //   }
-
-  //   document.querySelector(".column").innerHTML = semiDetails;
-  // });
-
   // ================= Country Grid ==============
 
   for (let i = 0; i < countries.length; i++) {
-    semiDetails += ` <div class="country-column">
-    <div class="img-box"><img src="${countries[i].flags.svg}" alt="countryFlag" height="100px" width="200px" /></div>
+    semiDetails += ` <div class="country-column ${countries[i].alpha3Code} ">
+    <div class="img-box"><img src="${countries[i].flag}" alt="countryFlag" height="100px" width="200px" /></div>
     <div class="country-details">
       <h3>${countries[i].name}</h3>
       <div>
@@ -80,11 +65,92 @@ function init(countriesData) {
   }
 
   document.querySelector(".column").innerHTML = semiDetails;
+
+  // ================= Country Details ==============
+
+  for (let i = 0; i < countries.length; i++) {
+    document
+      .querySelector(`.${countries[i].alpha3Code}`)
+      .addEventListener("click", function () {
+        fullDetails = `
+        <div class="big-flag"><img src="${
+          countries[i].flag
+        }" alt="big flag" width="450px" height="300px" /></div>
+       <div class="more-info">
+       <h1>${countries[i].name}</h1>
+        <div class="flex-details">
+        <div class="first-coln">
+          <div>
+            <label for="nativ-name">Native Name:</label>
+            <span>${countries[i].nativeName}</span>
+          </div>
+          <div>
+            <label for="population">Population:</label>
+            <span>${countries[i].population}</span>
+          </div>
+          <div>
+            <label for="region">Region:</label>
+            <span>${countries[i].region}</span>
+          </div>
+          <div>
+            <label for="sub-region">Sub Region:</label>
+            <span>${countries[i].subregion}</span>
+          </div>
+          <div>
+            <label for="capital">Capital:</label>
+            <span>${countries[i].capital}</span>
+          </div>
+        </div>
+        <div class="second-coln">
+          <div>
+            <label for="tld">Top Level Domain:</label>
+            <span>${countries[i].topLevelDomain}</span>
+          </div>
+          <div>
+            <label for="currencies">Currencies:</label>
+            <span>${countries[i].currencies[0].name}</span>
+          </div>
+          <div>
+            <label for="languages">Languages:</label>
+            <span> ${countries[i].languages.map((e) => `  ${e.name}`)}
+          </div>
+        </div>
+        </div>
+       <div class="border-countries">
+        <label for="border-count">Border Countries:</label>
+       
+       ${
+         countries[i].borders
+           ? `<div class="border-grid">
+
+        
+
+           ${[...countries[i].borders]
+             .map(
+               (e) =>
+                 `<div class="border">${[
+                   countries.filter((a) => a.alpha3Code == e),
+                 ].map((e) => e[0].name)}</div>`
+             )
+             .join("")}  
+           </div>`
+           : "None"
+       }       
+          </div>
+        
+       `;
+
+        detailed.innerHTML = fullDetails;
+        togglePage();
+      });
+  }
+
+  arrowBack.addEventListener("click", function () {
+    togglePage();
+  });
 }
 
-// fetch(`https://restcountries.com/v2/name/{name}`)
-//   .then((response) => response.json())
-//   .then((data2) => secondInit(data2))
-//   .catch((err) => console.error(err));
-
-// function secondInit(searchData) {}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+  }
+});
